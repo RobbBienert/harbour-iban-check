e003e390cc71bcca16eaee1139d56ba136239c27
Quent-in <pages_quentin@hotmail.com>
2016-08-05 21:13:56 +0200

    French local

---
+++ translations/harbour-iban-check-fr.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About IBAN Checker</source>
+        <translation>À propos d'IBAN Checker</translation>
+    </message>
+    <message>
+        <source>formal check for IBANs (International Bank Account Number)</source>
+        <translation>vérification simplifiée d'IBAN (International Bank Account Number)</translation>
+    </message>
+    <message>
+        <source>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.&lt;br&gt;This program is distributed in the hope that it will be useful, but &lt;strong&gt;without any warranty&lt;/strong&gt;; without even the implied warranty of &lt;strong&gt;merchantability&lt;/strong&gt; or &lt;strong&gt;fitness for a particular purpose&lt;/strong&gt;.</source>
+        <translation>Ce programme est un logiciel libre: vous pouvez le redistribuer et / ou le modifier selon les termes de la GNU General Public License telle que publiée par la Free Software Foundation, soit la version 3 de la licence, ou (à votre choix) toute version ultérieure & lt. ; br & gt; Ce programme est distribué dans l'espoir qu'il sera utile, mais & lt; strong & gt; sans aucune garantie & lt; / strong & gt;; sans même la garantie implicite de & lt; strong & gt; commercialité & lt; / strong & gt; ou & lt; strong & gt; adéquation à un usage particulier & lt; / strong & gt;.</translation>
+    </message>
+    <message>
+        <source>See the GNU General Public License for more details.</source>
+        <translation>Voir le licence générale publique GNU pour plus de détails.</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>IBAN Checker</source>
+        <translation>IBAN Checker</translation>
+    </message>
+</context>
+<context>
+    <name>HelpPage</name>
+    <message>
+        <source>IBAN Checker Help</source>
+        <translation>Aide d'IBAN Checker</translation>
+    </message>
+    <message>
+        <source>This app helps you for a quick (but only formal!) check of your IBAN.</source>
+        <translation>Cette application vous permet de vérifier, formellement, des IBAN.</translation>
+    </message>
+    <message>
+        <source>You can either fill in a complete IBAN (copy &amp; paste) or create it step-by-step by selecting country, checksum and bank account number. If the checksum is not filled in (or is set to either &apos;00&apos; or &apos;xx&apos;) it is calculated.</source>
+        <translation>Vous pouvez soit rentrer un IBAN complet (copier-coller) ou le créer pas à pas en choisissant le pays, la clef et le code banque. Si la clef n'est pas renseignée (ou si elle est remplie avec &apos;00&apos; or &apos;xx&apos;) elle sera calculée.</translation>
+    </message>
+    <message>
+        <source>https://en.wikipedia.org/wiki/International_Bank_Account_Number</source>
+        <translation>https://fr.wikipedia.org/wiki/International_Bank_Account_Number</translation>
+    </message>
+    <message>
+        <source>Wikipedia: IBAN</source>
+        <translation>Wikipédia : IBAN</translation>
+    </message>
+    <message>
+        <source>Formal Check</source>
+        <translation>Vérification formelle</translation>
+    </message>
+    <message>
+        <source>Testing Functions</source>
+        <translation>Fonctions de test</translation>
+    </message>
+    <message>
+        <source>The IBAN check is only done by checking the number against the given checksum using the algorithm described in ISO 13616-1:2007. The actual validity can only be checked by banks and finance institutes.&lt;br&gt;For details see e. g.</source>
+        <translation>La vérification de l'IBAN se fait seulement en vérifiant le nombre avec la somme de contrôle en utilisant l'algorithme ISO 13616-1:2007. La bonne validité peut être vérifiée par les banques et les instituts financiers. Pour plus de détails voir e. g.</translation>
+    </message>
+    <message>
+        <source>As mentioned above, you can calculate the checksum by leaving the input empty or fill it with &apos;00&apos; or &apos;xx&apos;.
+For further testing without using real bank account numbers, there is also a example country &apos;XX&apos; at the end of the long list of countries.</source>
+        <translation>Comme indiqué, vous pouvez calculer la clef en laissant le champs texte vide ou en le remplissant avec &apos;00&apos; or &apos;xx&apos;.</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>About …</source>
+        <translation>À propos…</translation>
+    </message>
+    <message>
+        <source>IBAN Checker</source>
+        <translation>IBAN Checker</translation>
+    </message>
+    <message>
+        <source>Country</source>
+        <translation>Pays</translation>
+    </message>
+    <message>
+        <source>Checksum</source>
+        <translation>Clef</translation>
+    </message>
+    <message>
+        <source>Bank Account Number</source>
+        <translation>Numéro de compte</translation>
+    </message>
+    <message>
+        <source>Check</source>
+        <translation>Vérifier</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>Valide</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <source>(Copy &amp; Paste) IBAN</source>
+        <translation>(Copier-Coller) IBAN</translation>
+    </message>
+    <message>
+        <source> … or fill in the following fields:</source>
+        <translation>… ou remplissez les champs suivants : </translation>
+    </message>
+    <message>
+        <source>incorrect</source>
+        <translation>incorrect</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage2</name>
+    <message>
+        <source>About …</source>
+        <translation>À propos…</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <source>IBAN Checker</source>
+        <translation>IBAN Checker</translation>
+    </message>
+    <message>
+        <source>(Copy &amp; Paste) IBAN</source>
+        <translation>(Copier-Coller) IBAN</translation>
+    </message>
+    <message>
+        <source> … or fill in the following fields:</source>
+        <translation>… ou remplissez les champs suivants :</translation>
+    </message>
+    <message>
+        <source>Bank Account Number</source>
+        <translation>Code banque</translation>
+    </message>
+    <message>
+        <source>Check</source>
+        <translation>Vérifier</translation>
+    </message>
+    <message>
+        <source>IBAN</source>
+        <translation>IBAN</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>valide</translation>
+    </message>
+    <message>
+        <source>invalid</source>
+        <translation>incorrect</translation>
+    </message>
+    <message>
+        <source>Country</source>
+        <translation>Pays</translation>
+    </message>
+    <message>
+        <source>Checksum</source>
+        <translation>Clef</translation>
+    </message>
+</context>
+</TS>
