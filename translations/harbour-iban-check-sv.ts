6d3ef13292c9694813c99124ff1b892ca04d2cde
Åke Engelbrektson <eson57@users.noreply.github.com>
2016-08-06 13:41:47 +0200

    Create harbour-iban-check-sv.ts
    Swedish translation if you want.

---
+++ translations/harbour-iban-check-sv.ts
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About IBAN Checker</source>
+        <translation>Om IBAN Checker</translation>
+    </message>
+    <message>
+        <source>formal check for IBANs (International Bank Account Number)</source>
+        <translation>formell kontroll av IBAN-nummer (Internationella bankkontonummer)</translation>
+    </message>
+    <message>
+        <source>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.&lt;br&gt;This program is distributed in the hope that it will be useful, but &lt;strong&gt;without any warranty&lt;/strong&gt;; without even the implied warranty of &lt;strong&gt;merchantability&lt;/strong&gt; or &lt;strong&gt;fitness for a particular purpose&lt;/strong&gt;.</source>
+        <translation>Detta program är fri mjukvara! Du kan återdistribuera och/eller modifera den under vilkoren för GNU General Public License som publicerad av Free Software Foundation, antingen version 3 av licensen, eller (om så önskas) någon senare version.&lt;br&gt;Programmet distribueras med hopp om att det skall vara användbart, men &lt;strong&gt;utan några garantier&lt;/strong&gt;; utan ens underförstådd garanti för &lt;strong&gt;säljbarhet&lt;/strong&gt; eller &lt;strong&gt;lämplighet för särskilt ändamål&lt;/strong&gt;.</translation>
+    </message>
+    <message>
+        <source>See the GNU General Public License for more details.</source>
+        <translation>Se GNU General Public License för mer info.</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>IBAN Checker</source>
+        <translation>IBAN Checker</translation>
+    </message>
+</context>
+<context>
+    <name>HelpPage</name>
+    <message>
+        <source>IBAN Checker Help</source>
+        <translation>IBAN Checker Hjälp</translation>
+    </message>
+    <message>
+        <source>This app helps you for a quick (but only formal!) check of your IBAN.</source>
+        <translation>Denna app hjälper dig med en snabb (men formell) kontroll av IBAN-nummer.</translation>
+    </message>
+    <message>
+        <source>You can either fill in a complete IBAN (copy &amp; paste) or create it step-by-step by selecting country, checksum and bank account number. If the checksum is not filled in (or is set to either &apos;00&apos; or &apos;xx&apos;) it is calculated.</source>
+        <translation>Du kan antingen ange ett komplett IBAN-nummer (kopiera &amp; klistra in) eller skapa det stegvis, genom att välja land, kontrollsumma och bankkontonummer. Om kontrollsumman inte är ifylld (eller angiven som &apos;00&apos; eller &apos;xx&apos;), kommer den att beräknas.</translation>
+    </message>
+    <message>
+        <source>https://en.wikipedia.org/wiki/International_Bank_Account_Number</source>
+        <translation>https://en.wikipedia.org/wiki/International_Bank_Account_Number</translation>
+    </message>
+    <message>
+        <source>Wikipedia: IBAN</source>
+        <translation>Wikipedia: IBAN</translation>
+    </message>
+    <message>
+        <source>Formal Check</source>
+        <translation>Formell kontroll</translation>
+    </message>
+    <message>
+        <source>Testing Functions</source>
+        <translation>Testfunktioner</translation>
+    </message>
+    <message>
+        <source>The IBAN check is only done by checking the number against the given checksum using the algorithm described in ISO 13616-1:2007. The actual validity can only be checked by banks and finance institutes.&lt;br&gt;For details see e. g.</source>
+        <translation>IBAN-kontrollen görs endast genom att kontrollera nummret mot given kontrollsumma, med hjälp av algoritmen beskriven i ISO 13616-1:2007. Verklig validitet kan endast kontrolleras av banker och finansiella institut.&lt;br&gt;Se exempel för detaljerad info.</translation>
+    </message>
+    <message>
+        <source>As mentioned above, you can calculate the checksum by leaving the input empty or fill it with &apos;00&apos; or &apos;xx&apos;.
+For further testing without using real bank account numbers, there is also a example country &apos;XX&apos; at the end of the long list of countries.</source>
+        <translation>Som nämnts ovan, kan du beräkna kontrollsumman genom att lämna fältet tomt, eller fylla i det med &apos;00&apos; eller &apos;xx&apos;.
+För vidare testning utan att använda riktiga bankkontonummer, finns det också ett land &apos;XX&apos; i slutet på nationslistan.</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>About …</source>
+        <translation>Om ...</translation>
+    </message>
+    <message>
+        <source>IBAN Checker</source>
+        <translation>IBAN Checker</translation>
+    </message>
+    <message>
+        <source>Country</source>
+        <translation>Land</translation>
+    </message>
+    <message>
+        <source>Checksum</source>
+        <translation>Kontrollsumma</translation>
+    </message>
+    <message>
+        <source>Bank Account Number</source>
+        <translation>Bankkontonummer</translation>
+    </message>
+    <message>
+        <source>Check</source>
+        <translation>Kontrollera</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Hjälp</translation>
+    </message>
+    <message>
+        <source>(Copy &amp; Paste) IBAN</source>
+        <translation>(Kopiera &amp; Klistra in) IBAN</translation>
+    </message>
+    <message>
+        <source> … or fill in the following fields:</source>
+        <translation> ... eller fyll i följande fält:</translation>
+    </message>
+    <message>
+        <source>incorrect</source>
+        <translation>Felaktigt</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage2</name>
+    <message>
+        <source>About …</source>
+        <translation>Om ...</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Hjälp</translation>
+    </message>
+    <message>
+        <source>IBAN Checker</source>
+        <translation>IBAN Checker</translation>
+    </message>
+    <message>
+        <source>(Copy &amp; Paste) IBAN</source>
+        <translation>(Kopiera &amp; Klistra in) IBAN</translation>
+    </message>
+    <message>
+        <source> … or fill in the following fields:</source>
+        <translation> ... eller fyll i följande fält:</translation>
+    </message>
+    <message>
+        <source>Bank Account Number</source>
+        <translation>Bankkontonummer</translation>
+    </message>
+    <message>
+        <source>Check</source>
+        <translation>Kontrollera</translation>
+    </message>
+    <message>
+        <source>IBAN</source>
+        <translation>IBAN</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>invalid</source>
+        <translation>ogiltigt</translation>
+    </message>
+    <message>
+        <source>Country</source>
+        <translation>Land</translation>
+    </message>
+    <message>
+        <source>Checksum</source>
+        <translation>Kontrollsumma</translation>
+    </message>
+</context>
+</TS>
